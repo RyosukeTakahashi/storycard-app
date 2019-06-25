899f9d87f0ff9af09eb36646d0f0a357fc5720b1
Ryo Murakami <ryo.a.murakami@dena.com>
2019-06-25 16:13:19 +0900

    tried keyframes but maybe not good solution.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 /** @jsx jsx */
-import React from "react";
+import React, { useState } from "react";
 
-import { jsx, css } from "@emotion/core";
+import { jsx, css, keyframes } from "@emotion/core";
 
 import Button from "@material-ui/core/Button";
 
@@ -17,6 +17,13 @@ const phrases = [
   "いいかえると"
 ];
 
+//todo カード画像？要素？
+//todo 文字移動？
+//todo 山札の画像とHTML
+
+//todo ログをRailsに残せる
+//todo 語群ををRailsから取れる
+
 function Header() {
   return (
     <header>
@@ -31,7 +38,36 @@ function Header() {
   );
 }
 
-const PhraseSection = props => {
+// const bounce = keyframes`
+//   from, 20%, 53%, 80%, to {
+//     transform: translate3d(0,0,0);
+//   }
+//
+//   40%, 43% {
+//     transform: translate3d(-30px, -30px, 0);
+//   }
+//
+//   70% {
+//     transform: translate3d(-15px, -15px, 0);
+//   }
+//
+//   90% {
+//     transform: translate3d(-4px,-4px,0);
+//   }
+// `;
+
+const slideLeft = keyframes`
+  0% {
+    transform: translate3d(0, 0, 0);
+  }
+
+  100% {
+    transform: translate3d(-60vh, 0, 0);
+  }
+`;
+
+
+const PhraseSection = ({ phrase }) => {
   return (
     <div
       css={css`
@@ -50,7 +86,13 @@ const PhraseSection = props => {
         font-size: 3em;
       `}
     >
-      {props.phrase}
+      <p
+        css={css`
+          animation: ${slideLeft} 1s ease;
+        `}
+      >
+        {phrase}
+      </p>
     </div>
   );
 };
@@ -68,23 +110,38 @@ const EmotionButton = props => {
         justify-content: center;
       `}
     >
-      <Button variant="contained" color="primary">
+      <Button variant="contained" color="primary" onClick={props.onButtonClick}>
         次のカード
       </Button>
     </div>
   );
 };
 
+// const PhraseLog = ({ usedPhrases }) => {
+//   return <div>{usedPhrases.slice(0, usedPhrases.length - 1).join(",")}</div>;
+// };
+
 function App() {
-  const phrase = phrases[Math.floor(Math.random() * phrases.length)];
+  const getRandomPhrase = () => {
+    return phrases[Math.floor(Math.random() * phrases.length)];
+  };
 
+  const initialPhrase = getRandomPhrase();
+  const [phrase, setPhrase] = useState(initialPhrase);
+  const [phraseLog, addToPhraseLog] = useState([initialPhrase]);
 
+  function handleClick() {
+    const newPhrase = getRandomPhrase();
+    setPhrase(newPhrase);
+    addToPhraseLog(phraseLog.concat([newPhrase]));
+  }
 
   return (
     <div className="App">
       <Header />
       <PhraseSection phrase={phrase} />
-      <EmotionButton />
+      <EmotionButton onButtonClick={handleClick} />
+      {/*<PhraseLog usedPhrases={phraseLog}/>*/}
     </div>
   );
 }
